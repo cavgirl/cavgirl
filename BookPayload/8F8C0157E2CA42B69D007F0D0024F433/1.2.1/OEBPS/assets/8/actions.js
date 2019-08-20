cf8ef5e937845970ea7f39dc5b8e4e0f9648b6ba
cavgirl <47758879+cavgirl@users.noreply.github.com>
2019-08-20 19:03:05 +0100

    ul

---
+++ BookPayload/8F8C0157E2CA42B69D007F0D0024F433/1.2.1/OEBPS/assets/8/actions.js
@@ -0,0 +1,162 @@
+pubcoder.projectID = "18658B6D99D44CD8BE3D25481D40B4A6";
+pubcoder.project.id = "18658B6D99D44CD8BE3D25481D40B4A6";
+pubcoder.project.title = "Lisa Townsend\'s Project";
+pubcoder.page.id = 2052;
+pubcoder.page.title = "8";
+pubcoder.page.number = 8;
+pubcoder.page.alias = "";
+
+var ua = navigator.userAgent.toLowerCase();
+var touchDownEvent;
+var touchUpEvent;
+var isMobile;
+var aigX = 0, aigY = 0;
+var askAudioPermission = false;
+
+/*
+ * 
+ * Init Action Lists
+ *
+ * 
+ */
+
+/*
+ * 
+ * Init SCCounter
+ *
+ * 
+ */
+ 
+ $(window).load(function(){
+	window.eventObj = {};
+	/*
+	 * 
+	 * Init SCAnimation
+	 * 
+	 * 
+	 */
+	
+	/*
+	 *
+	 *   Init Shake
+	 *
+	 */
+	window.addEventListener('shake', function () {
+		
+	}, false);
+	
+	/*
+	 *
+	 *   Init Masked Images
+	 *
+	 */
+	 
+ 	/*
+	 * 
+	 * Init SCPhotogallery
+	 * 
+	 * 
+	 */
+	
+ 	/*
+	 * 
+	 * Init SCQuizMulti
+	 * 
+	 * 
+	 */
+	
+ 	/*
+	 * 
+	 * Init SCDrawer
+	 * 
+	 * 
+	 */
+	
+    
+ 	/*
+	 * 
+	 * Init SCWPanZoom
+	 * 
+	 * 
+	 */
+	
+    
+ 	/*
+	 * 
+	 * Init SCWMemoryGame
+	 * 
+	 * 
+	 */
+	
+    
+    
+	if(! navigator.userAgent.match(/PubCoderHelper/i)) {
+		/*
+		 *
+	 	 *   Action Groups
+	 	 *
+	 	 */
+		
+		
+		/*
+		 *
+	 	 *  Events
+	 	 *
+	 	 */
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		/*
+		 *
+	 	 *  Page is ready to be played
+	 	 *
+	 	 */
+		if (askAudioPermission) {
+	$("#SCAudioPermissionAllowButton").click(function () {
+		$( "audio" ).each(function( index ) {
+			this.play();
+			this.pause();
+		});
+		$(window).trigger(PubCoder.Events.PagePlay);
+	});
+	$("#SCAudioPermissionCancelButton").click(function () {
+		$(window).trigger(PubCoder.Events.PagePlay);
+	});
+	var remodalInst = $('[data-remodal-id=SCAudioPermissionAlert]').remodal().open();
+} else {
+	setTimeout(function() {
+		$(window).trigger(PubCoder.Events.PagePlay);
+	}, 200);
+}
+	 }
+});
+var orientationDevice = getOrientation(PubCoder.isAndroidReader);
+$(window).on(PubCoder.Events.PagePlay, function() {
+	
+$("#obj2053").trigger('SCEventShow');
+$("#obj2069").trigger('SCEventShow');
+$("#obj2072").trigger('SCEventShow');
+	$(window).trigger(PubCoder.Events.PageReady);
+	
+});
